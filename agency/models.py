e69ec3d2835240aef26f54820ab58eff1326a713
Maksym Omelkov <omelkov85@gmail.com>
2025-04-16 15:47:39 +0300

    added model classes

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(
+        "Topic",
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    redactors = models.ManyToManyField(
+        Redactor,
+        related_name="newspapers"
+    )
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100, unique=True)
