13fecc492e48d26be008924583ae3b755a4f70bf
Maksym Omelkov <omelkov85@gmail.com>
2025-04-26 04:57:29 +0300

    updated changes for deploy

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "newspaper-agency-m9df.onrender.com", "*"]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
